6c26c616be985e4422923cb04f004b6c6779a080
Guillermo Valdes <guillermo@movimientolibre.com>
2025-06-12 09:06:03 -0600

    Corregi modelo web pagina a tipo JSONB

--- pjecz_casiopea_flask/blueprints/web_paginas/models.py
+++ pjecz_casiopea_flask/blueprints/web_paginas/models.py
@@ -52,7 +52,7 @@ class WebPagina(database.Model, UniversalMixin):
     # Columnas contenido
     contenido_html: Mapped[Optional[str]] = mapped_column(Text)
     contenido_md: Mapped[Optional[str]] = mapped_column(Text)  # CKEditor5
-    contenido_sfdt: Mapped[Optional[JSONB]] = mapped_column(Text)  # Syncfusion Document Editor
+    contenido_sfdt: Mapped[Optional[JSONB]] = mapped_column(JSONB)  # Syncfusion Document Editor
 
     # Hijos
     web_archivos: Mapped[List["WebArchivo"]] = relationship("WebArchivo", back_populates="web_pagina")
